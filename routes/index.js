04b34865c9003597a9e7bcd2746515813f447db7
Kiran Shagoti <kiranmshagoti@Kirans-MacBook-Air.local>
2019-09-17 10:55:38 +0200

    agreed to changes on dashboard

--- routes/index.js
+++ routes/index.js
@@ -19,6 +19,8 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+
+//To get all the information and every existing task 
 router.get("/dashboard", loginCheck(), (req, res, next) => {
   // task from the board
   Board.find().then(boards=> {
@@ -26,6 +28,8 @@ router.get("/dashboard", loginCheck(), (req, res, next) => {
   })  
 });
 
+
+//to create and post the tasks into the database and redirect back to dashboard-refresh the page
 router.post("/dashboard", loginCheck(), (req, res, next) => {
   Board.create({ name: req.body.taskName, description: req.body.toDo }).then(() => {
     res.redirect('/dashboard')
